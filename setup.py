a6338cc543b6491479bf3932b1dabd8b592bc622
Noah Costello <29776732+nmcostello@users.noreply.github.com>
2024-11-04 15:53:29 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='stock',
+    version='0.1.0',
+    description='A example Python package',
+    url='https://github.com/nmcostello/stock',
+    author='Noah Costello',
+    author_email='me@noahcostello.io',
+    license='BSD 2-clause',
+    packages=['stock'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
